3ae9947328b5b51e53dcb254d3bcda127c6e86fd
Preslav977 <p.cvetanow@gmail.com>
2023-02-19 10:44:10 +0200

    Created function that loops the array and creates cards to display the books

--- script.js
+++ script.js
@@ -24,3 +24,35 @@ function addBookToLibrary() {
 }
 
 addBookToLibrary();
+
+function addBookToCard(bookObject) {
+  // objects needs to be passed just to create
+  // a book based on that
+  // if bookObject is passed here create
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const book of myLibrary) {
+    // loop the array and call that function
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card");
+    cardBookContainer.appendChild(cardContainer);
+
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.add("card-content");
+    titleDiv.innerText = `Title: ${book.title}`;
+    cardContainer.appendChild(titleDiv);
+    const authorDiv = document.createElement("div");
+    authorDiv.classList.add("card-content");
+    authorDiv.innerText = `Author: ${book.author}`;
+    cardContainer.appendChild(authorDiv);
+    const pagesDiv = document.createElement("div");
+    pagesDiv.classList.add("card-content");
+    pagesDiv.innerText = `Pages: ${book.pages}`;
+    cardContainer.appendChild(pagesDiv);
+    const readDiv = document.createElement("div");
+    readDiv.classList.add("card-content");
+    readDiv.innerText = `Read: ${book.read}`;
+    cardContainer.appendChild(readDiv);
+  }
+  return bookObject;
+}
